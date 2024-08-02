033ecd657b776b63e7d48b9bdb0e00c3459220c4
Baivab Bhusal <baivabbhusal229@gmail.com>
2024-08-02 20:43:13 +0545

    Add folder

---
+++ Day_9.js
@@ -0,0 +1,8 @@
+//Activity 1--Selecting and Manupulating Element
+
+let heading=document.getElementById("heading");
+console.log(heading);
+heading.innerHTML="I am Baivab";
+
+let headers=document.getElementsByClassName("header");
+headers.style.backgroundColor="green";
